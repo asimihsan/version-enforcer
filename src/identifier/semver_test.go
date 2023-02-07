e51bc03a55f9d15a195064e1cb70d258b9d84e75
Asim Ihsan <ssh@sshemail.ihsan.io>
2023-02-07 12:43:17 -0800

    fix license header

--- src/identifier/semver_test.go
+++ src/identifier/semver_test.go
@@ -1,7 +1,7 @@
 /*
  * Copyright 2023 Asim Ihsan
  *
- * Licensed under the Apache License, SemverVersion 2.0 (the "License");
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
